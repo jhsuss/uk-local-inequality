2d487cfeba413c7eb05f7218f98ec1c14de4623f
Joel Suss <suss.joel@gmail.com>
2023-02-13 16:22:45 +0000

    paper published update

--- R/global.R
+++ R/global.R
@@ -1,12 +1,30 @@
+library(mapdeck)
+library(tidyverse)
+
 load("data/data_clean.Rdata")
 
-library(mapdeck)
-# TODO how would this work on github or shiny server?
+map_data <- map_data %>% 
+  map(
+    rename,
+    "Gini" = "gini",
+    "Top 1% share" = "top1", # TODO reveals outlier in NI LAD
+    #"Skew" = "skew", # TODO reveals a lot of outliers? 
+    "90:10 ratio" = "nine_ten",
+    "90:50 ratio" = "nine_five",
+    "50:10 ratio" = "five_ten",
+    "Average house price" = "avg",
+    "Coefficient of variation" = "cv"
+  ) %>% 
+  map(
+    mutate,
+    med = round(med)
+  )
+
+# remember to delete actual key before pushing to github 
 # set API token for mapbox 
 set_token(Sys.getenv("mapbox_public_key"))
 
 # spatial level dropdown choices
-# TODO add LAD, TTWA
 levs <- c(
   "LSOA" = "lsoa",
   "MSOA" = "msoa",
@@ -19,13 +37,14 @@ levs <- c(
 # inequality measure choices
 # TODO add CV, skew, 90:10, etc
 ineq_measures <- c(
-  "Gini" = "gini",
-  "Top 1%" = "top1", # TODO reveals outlier in NI LAD
+  "Gini",
+  "Top 1% share", # TODO reveals outlier in NI LAD
   #"Skew" = "skew", # TODO reveals a lot of outliers? 
-  "90:10 ratio" = "nine_ten",
-  "90:50 ratio" = "nine_five",
-  "50:10 ratio" = "five_ten",
-  "Coefficient of variation" = "cv"
+  "90:10 ratio",
+  "90:50 ratio",
+  "50:10 ratio",
+  "Average house price",
+  "Coefficient of variation"
 )
 
 # TODO in options add data source: "housing", "income", "wealth"
